1fb3593d46936323339c52aac800b03b77237bcc
YHANJARAMILLOD <yhjaramillod@unal.edu.co>
2024-11-12 01:51:50 -0500

    vuelve y juega

--- src/test/java/test/Motor.java
+++ src/test/java/test/Motor.java
@@ -4,7 +4,7 @@ public class Motor {
     String tipo;
     int registro;
     void cambiarRegistro(int registro){
-        this.registro=++registro;
+        this.registro=registro;
     }
     void asignarTipo(String tipo){
         if (tipo=="gasolina" || tipo=="electrico"){
